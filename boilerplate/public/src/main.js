433df908065b5f72bf1b264b00932e44988622a8
Roger Madjos <roger.madjos@gmail.com>
2016-12-10 02:35:55 +0800

    make socket object global

--- boilerplate/public/src/main.js
+++ boilerplate/public/src/main.js
@@ -5,8 +5,6 @@ import App from './App.js';
 let root = document.getElementById('root');
 ReactDOM.render(<App message={'Hello World!'}/>, root);
 
-var socket = io('http://localhost:8080');
-
 socket.on('connect', function (data) {
   ReactDOM.render(<App message={'Connected'}/>, root);
 });
